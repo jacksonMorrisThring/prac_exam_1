ef966306330aec7f475384961528efc0ede4ea12
Jackson Morris-Thring <jacksonmorris-thring@student-10-201-32-037.wireless.adelaide.edu.au>
2022-08-24 14:05:12 +0930

    1-1 retry

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -5,6 +5,7 @@ void print_ascending(int *vals, int len){
     {
         if (i > 0){
             if (*(vals + i) < *(vals + i - 1)){
+                std::cout << std::endl;
                 break;
             }
             else{
